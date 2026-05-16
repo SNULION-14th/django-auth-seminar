c659b1bd8ade3a0537564fe36fc22440df6681b4
hanjoonbae <hanjoonbae.jobis.co>
2026-05-16 16:54:16 +0900

    homework

--- account/admin.py
+++ account/admin.py
@@ -1,2 +1,15 @@
+from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from .models import User
 
-# Register your models here.
+@admin.register(User)
+class CustomUserAdmin(UserAdmin):
+    list_display = ('username', 'college', 'major', 'is_staff')
+
+    fieldsets = UserAdmin.fieldsets + (
+        ('Additional Info', {'fields': ('college', 'major')}),
+    )
+    
+    add_fieldsets = UserAdmin.add_fieldsets + (
+        ('Additional Info', {'fields': ('college', 'major')}),
+    )
--- account/request_serializers.py
+++ account/request_serializers.py
@@ -12,3 +12,9 @@ class SignUpRequestSerializer(serializers.Serializer):
 class SignInRequestSerializer(serializers.Serializer):
     username = serializers.CharField()
     password = serializers.CharField()
+
+class TokenRefreshRequestSerializer(serializers.Serializer):
+    refresh = serializers.CharField()
+
+class SignOutRequestSerializer(serializers.Serializer):
+    refresh = serializers.CharField()
