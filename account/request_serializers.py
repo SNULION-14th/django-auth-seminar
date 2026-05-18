24cf8848e28ee1faa6a7b08344319eb718824e88
Suhyeon An <sh-an@naver.com>
2026-05-18 23:17:44 +0900

    add logout api

--- account/request_serializers.py
+++ account/request_serializers.py
@@ -15,3 +15,6 @@ class SignInRequestSerializer(serializers.Serializer):
 
 class TokenRefreshRequestSerializer(serializers.Serializer):
     refresh = serializers.CharField()
+
+class LogOutRequestSerializer(serializers.Serializer):
+    refresh = serializers.CharField()
